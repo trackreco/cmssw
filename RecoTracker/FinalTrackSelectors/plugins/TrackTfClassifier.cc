3c47354a028f5a5b1a629f996be73ec59cef6bb3
leonardo <leonardo.giannini@cern.ch>
2022-05-13 18:54:50 -0700

    cleanup and add function

--- RecoTracker/FinalTrackSelectors/plugins/TrackTfClassifier.cc
+++ RecoTracker/FinalTrackSelectors/plugins/TrackTfClassifier.cc
@@ -11,6 +11,12 @@
 #include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
 #include "RecoTracker/FinalTrackSelectors/interface/TfGraphDefWrapper.h"
 
+void trkFiller(tensorflow::Tensor& input1,
+               int const& n,
+               reco::Track const& trk,
+               reco::BeamSpot const& beamSpot,
+               math::XYZPoint const& bestVertex);
+
 namespace {
   class TfDnn {
   public:
@@ -42,37 +48,7 @@ namespace {
       tensorflow::Tensor input1(tensorflow::DT_FLOAT, {1, 29});
       tensorflow::Tensor input2(tensorflow::DT_FLOAT, {1, 1});
 
-      input1.matrix<float>()(0, 0) = trk.pt();
-      input1.matrix<float>()(0, 1) = trk.innerMomentum().x();
-      input1.matrix<float>()(0, 2) = trk.innerMomentum().y();
-      input1.matrix<float>()(0, 3) = trk.innerMomentum().z();
-      input1.matrix<float>()(0, 4) = trk.innerMomentum().rho();
-      input1.matrix<float>()(0, 5) = trk.outerMomentum().x();
-      input1.matrix<float>()(0, 6) = trk.outerMomentum().y();
-      input1.matrix<float>()(0, 7) = trk.outerMomentum().z();
-      input1.matrix<float>()(0, 8) = trk.outerMomentum().rho();
-      input1.matrix<float>()(0, 9) = trk.ptError();
-      input1.matrix<float>()(0, 10) = trk.dxy(bestVertex);
-      input1.matrix<float>()(0, 11) = trk.dz(bestVertex);
-      input1.matrix<float>()(0, 12) = trk.dxy(beamSpot.position());
-      input1.matrix<float>()(0, 13) = trk.dz(beamSpot.position());
-      input1.matrix<float>()(0, 14) = trk.dxyError();
-      input1.matrix<float>()(0, 15) = trk.dzError();
-      input1.matrix<float>()(0, 16) = trk.normalizedChi2();
-      input1.matrix<float>()(0, 17) = trk.eta();
-      input1.matrix<float>()(0, 18) = trk.phi();
-      input1.matrix<float>()(0, 19) = trk.etaError();
-      input1.matrix<float>()(0, 20) = trk.phiError();
-      input1.matrix<float>()(0, 21) = trk.hitPattern().numberOfValidPixelHits();
-      input1.matrix<float>()(0, 22) = trk.hitPattern().numberOfValidStripHits();
-      input1.matrix<float>()(0, 23) = trk.ndof();
-      input1.matrix<float>()(0, 24) = trk.hitPattern().numberOfLostTrackerHits(reco::HitPattern::MISSING_INNER_HITS);
-      input1.matrix<float>()(0, 25) = trk.hitPattern().numberOfLostTrackerHits(reco::HitPattern::MISSING_OUTER_HITS);
-      input1.matrix<float>()(0, 26) =
-          trk.hitPattern().trackerLayersTotallyOffOrBad(reco::HitPattern::MISSING_INNER_HITS);
-      input1.matrix<float>()(0, 27) =
-          trk.hitPattern().trackerLayersTotallyOffOrBad(reco::HitPattern::MISSING_OUTER_HITS);
-      input1.matrix<float>()(0, 28) = trk.hitPattern().trackerLayersWithoutMeasurement(reco::HitPattern::TRACK_HITS);
+      trkFiller(input1, 0, trk, beamSpot, bestVertex);
 
       //Original algo as its own input, it will enter the graph so that it gets one-hot encoded, as is the preferred
       //format for categorical inputs, where the labels do not have any metric amongst them
@@ -125,61 +101,25 @@ namespace {
     std::vector<float> operator()(reco::TrackCollection const& tracks,
                                   reco::BeamSpot const& beamSpot,
                                   reco::VertexCollection const& vertices) const {
-      //std::cout <<"in batch eval part"<< std::endl;
-
       int size_in = (int)tracks.size();
-      int bsize = bsize_;
-      int nbatches = size_in / bsize;
+      int nbatches = size_in / bsize_;
 
       std::vector<float> output;
       output.resize(size_in);
 
-      tensorflow::Tensor input1(tensorflow::DT_FLOAT, {bsize, 29});
-      tensorflow::Tensor input2(tensorflow::DT_FLOAT, {bsize, 1});
+      tensorflow::Tensor input1(tensorflow::DT_FLOAT, {bsize_, 29});
+      tensorflow::Tensor input2(tensorflow::DT_FLOAT, {bsize_, 1});
 
       for (auto nb = 0; nb < nbatches + 1; nb++) {
-        for (auto nt = 0; nt < bsize; nt++) {
-          int itrack = nt + bsize * nb;
+        for (auto nt = 0; nt < bsize_; nt++) {
+          int itrack = nt + bsize_ * nb;
           if (itrack >= size_in)
             continue;
           const auto& trk = tracks.at(itrack);
 
           const auto& bestVertex = getBestVertex(trk, vertices);
 
-          input1.matrix<float>()(nt, 0) = trk.pt();
-          input1.matrix<float>()(nt, 1) = trk.innerMomentum().x();
-          input1.matrix<float>()(nt, 2) = trk.innerMomentum().y();
-          input1.matrix<float>()(nt, 3) = trk.innerMomentum().z();
-          input1.matrix<float>()(nt, 4) = trk.innerMomentum().rho();
-          input1.matrix<float>()(nt, 5) = trk.outerMomentum().x();
-          input1.matrix<float>()(nt, 6) = trk.outerMomentum().y();
-          input1.matrix<float>()(nt, 7) = trk.outerMomentum().z();
-          input1.matrix<float>()(nt, 8) = trk.outerMomentum().rho();
-          input1.matrix<float>()(nt, 9) = trk.ptError();
-          input1.matrix<float>()(nt, 10) = trk.dxy(bestVertex);
-          input1.matrix<float>()(nt, 11) = trk.dz(bestVertex);
-          input1.matrix<float>()(nt, 12) = trk.dxy(beamSpot.position());
-          input1.matrix<float>()(nt, 13) = trk.dz(beamSpot.position());
-          input1.matrix<float>()(nt, 14) = trk.dxyError();
-          input1.matrix<float>()(nt, 15) = trk.dzError();
-          input1.matrix<float>()(nt, 16) = trk.normalizedChi2();
-          input1.matrix<float>()(nt, 17) = trk.eta();
-          input1.matrix<float>()(nt, 18) = trk.phi();
-          input1.matrix<float>()(nt, 19) = trk.etaError();
-          input1.matrix<float>()(nt, 20) = trk.phiError();
-          input1.matrix<float>()(nt, 21) = trk.hitPattern().numberOfValidPixelHits();
-          input1.matrix<float>()(nt, 22) = trk.hitPattern().numberOfValidStripHits();
-          input1.matrix<float>()(nt, 23) = trk.ndof();
-          input1.matrix<float>()(nt, 24) =
-              trk.hitPattern().numberOfLostTrackerHits(reco::HitPattern::MISSING_INNER_HITS);
-          input1.matrix<float>()(nt, 25) =
-              trk.hitPattern().numberOfLostTrackerHits(reco::HitPattern::MISSING_OUTER_HITS);
-          input1.matrix<float>()(nt, 26) =
-              trk.hitPattern().trackerLayersTotallyOffOrBad(reco::HitPattern::MISSING_INNER_HITS);
-          input1.matrix<float>()(nt, 27) =
-              trk.hitPattern().trackerLayersTotallyOffOrBad(reco::HitPattern::MISSING_OUTER_HITS);
-          input1.matrix<float>()(nt, 28) =
-              trk.hitPattern().trackerLayersWithoutMeasurement(reco::HitPattern::TRACK_HITS);
+          trkFiller(input1, nt, trk, beamSpot, bestVertex);
 
           //Original algo as its own input, it will enter the graph so that it gets one-hot encoded, as is the preferred
           //format for categorical inputs, where the labels do not have any metric amongst them
@@ -197,8 +137,8 @@ namespace {
         //evaluate the input
         tensorflow::run(const_cast<tensorflow::Session*>(session_), inputs, {"Identity"}, &outputs);
 
-        for (auto nt = 0; nt < bsize; nt++) {
-          int itrack = nt + bsize * nb;
+        for (auto nt = 0; nt < bsize_; nt++) {
+          int itrack = nt + bsize_ * nb;
           if (itrack >= size_in)
             continue;
           float out0 = 2.0 * outputs[0].matrix<float>()(nt, 0) - 1.0;
@@ -213,10 +153,6 @@ namespace {
     const tensorflow::Session* session_;
     const int bsize_;
   };
-
-  //   using TrackTfClassifier = TrackMVAClassifier<TfDnn>;
-  //   using TrackTfClassifierBatch = TrackMVAClassifier<TfDnnBatch>;
-
 }  // namespace
 
 template <>
@@ -234,6 +170,42 @@ void trackMVAClassifierImpl::ComputeMVA<void>::operator()(::TfDnnBatch const& mv
   }
 }
 
+void trkFiller(tensorflow::Tensor& input1,
+               int const& n,
+               reco::Track const& trk,
+               reco::BeamSpot const& beamSpot,
+               math::XYZPoint const& bestVertex) {
+  input1.matrix<float>()(n, 0) = trk.pt();
+  input1.matrix<float>()(n, 1) = trk.innerMomentum().x();
+  input1.matrix<float>()(n, 2) = trk.innerMomentum().y();
+  input1.matrix<float>()(n, 3) = trk.innerMomentum().z();
+  input1.matrix<float>()(n, 4) = trk.innerMomentum().rho();
+  input1.matrix<float>()(n, 5) = trk.outerMomentum().x();
+  input1.matrix<float>()(n, 6) = trk.outerMomentum().y();
+  input1.matrix<float>()(n, 7) = trk.outerMomentum().z();
+  input1.matrix<float>()(n, 8) = trk.outerMomentum().rho();
+  input1.matrix<float>()(n, 9) = trk.ptError();
+  input1.matrix<float>()(n, 10) = trk.dxy(bestVertex);
+  input1.matrix<float>()(n, 11) = trk.dz(bestVertex);
+  input1.matrix<float>()(n, 12) = trk.dxy(beamSpot.position());
+  input1.matrix<float>()(n, 13) = trk.dz(beamSpot.position());
+  input1.matrix<float>()(n, 14) = trk.dxyError();
+  input1.matrix<float>()(n, 15) = trk.dzError();
+  input1.matrix<float>()(n, 16) = trk.normalizedChi2();
+  input1.matrix<float>()(n, 17) = trk.eta();
+  input1.matrix<float>()(n, 18) = trk.phi();
+  input1.matrix<float>()(n, 19) = trk.etaError();
+  input1.matrix<float>()(n, 20) = trk.phiError();
+  input1.matrix<float>()(n, 21) = trk.hitPattern().numberOfValidPixelHits();
+  input1.matrix<float>()(n, 22) = trk.hitPattern().numberOfValidStripHits();
+  input1.matrix<float>()(n, 23) = trk.ndof();
+  input1.matrix<float>()(n, 24) = trk.hitPattern().numberOfLostTrackerHits(reco::HitPattern::MISSING_INNER_HITS);
+  input1.matrix<float>()(n, 25) = trk.hitPattern().numberOfLostTrackerHits(reco::HitPattern::MISSING_OUTER_HITS);
+  input1.matrix<float>()(n, 26) = trk.hitPattern().trackerLayersTotallyOffOrBad(reco::HitPattern::MISSING_INNER_HITS);
+  input1.matrix<float>()(n, 27) = trk.hitPattern().trackerLayersTotallyOffOrBad(reco::HitPattern::MISSING_OUTER_HITS);
+  input1.matrix<float>()(n, 28) = trk.hitPattern().trackerLayersWithoutMeasurement(reco::HitPattern::TRACK_HITS);
+}
+
 namespace {
   using TrackTfClassifier = TrackMVAClassifier<TfDnn>;
   using TrackTfClassifierBatch = TrackMVAClassifier<TfDnnBatch>;
@@ -245,4 +217,3 @@ namespace {
 
 DEFINE_FWK_MODULE(TrackTfClassifier);
 DEFINE_FWK_MODULE(TrackTfClassifierBatch);
-
