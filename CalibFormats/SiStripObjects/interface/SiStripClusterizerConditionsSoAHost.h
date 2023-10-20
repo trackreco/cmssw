e99f9ab4b517de90af551b940190956e92cf53f3
Manos Vourliotis <emmanouil.vourliotis@gmail.com>
2023-10-20 02:00:34 -0700

    Restructure SoAs and first successful compilation

--- CalibFormats/SiStripObjects/interface/SiStripClusterizerConditionsSoAHost.h
+++ CalibFormats/SiStripObjects/interface/SiStripClusterizerConditionsSoAHost.h
@@ -2,9 +2,147 @@
 #define CalibFormats_SiStripObjects_SiStripClusterizerConditionsSoAHost_h
 
 #include "CUDADataFormats/Common/interface/PortableHostCollection.h"
-#include "CalibFormats/SiStripObjects/SiStripClusterizerConditionsSoAUtilities.h"
+#include "CalibFormats/SiStripObjects/interface/SiStripClusterizerConditionsSoAUtilities.h"
+#include "DataFormats/SiStripCluster/interface/SiStripClusterTools.h"
 
 namespace stripgpu {
+
+  //class SiStripClusterizerConditionsSoAHost {
+  //public:
+    class DetToFed {
+    public:
+      DetToFed(detId_t detid, apvPair_t ipair, fedId_t fedid, fedCh_t fedch)
+          : detid_(detid), ipair_(ipair), fedid_(fedid), fedch_(fedch) {}
+      detId_t detID() const { return detid_; }
+      apvPair_t pair() const { return ipair_; }
+      fedId_t fedID() const { return fedid_; }
+      fedCh_t fedCh() const { return fedch_; }
+
+    private:
+      detId_t detid_;
+      apvPair_t ipair_;
+      fedId_t fedid_;
+      fedCh_t fedch_;
+    };
+    //class DetToFedSoAHost : public cms::cuda::PortableHostCollection<DetToFedLayout> {
+    //public:
+    //  using cms::cuda::PortableHostCollection<DetToFedLayout>::view;
+    //  using cms::cuda::PortableHostCollection<DetToFedLayout>::const_view;
+    //  using cms::cuda::PortableHostCollection<DetToFedLayout>::buffer;
+    //  using cms::cuda::PortableHostCollection<DetToFedLayout>::bufferSize;
+    //  
+    //  DetToFedSoAHost() = default;
+    //  ~DetToFedSoAHost() = default;
+    //  
+    //  explicit DetToFedSoAHost(detId_t detid, apvPair_t ipair, fedId_t fedid, fedCh_t fedch, cudaStream_t stream)
+    //      : PortableHostCollection<DetToFedLayout>(
+    //          detID_(detid), pair_(ipair), fedID_(fedid), fedCh_(fedch)){};
+    //  
+    //  DetToFedSoAHost(const DetToFedSoAHost &&) = delete;
+    //  DetToFedSoAHost &operator=(const DetToFedSoAHost &&) = delete;
+    //  DetToFedSoAHost(DetToFedSoAHost &&) = default;
+    //  DetToFedSoAHost &operator=(DetToFedSoAHost &&) = default;
+    //};    
+    using DetToFeds = std::vector<DetToFed>;    
+
+    //const DetToFeds& detToFeds() const { return detToFeds_; }
+
+    class DataSoAHost : public cms::cuda::PortableHostCollection<DataLayout> {
+    public:
+      using cms::cuda::PortableHostCollection<DataLayout>::view;
+      using cms::cuda::PortableHostCollection<DataLayout>::const_view;
+      using cms::cuda::PortableHostCollection<DataLayout>::buffer;
+      using cms::cuda::PortableHostCollection<DataLayout>::bufferSize;
+      
+      DataSoAHost() = default;
+      ~DataSoAHost() = default;
+
+      DataSoAHost(const DataSoAHost &&) = delete;
+      DataSoAHost &operator=(const DataSoAHost &&) = delete;
+      DataSoAHost(DataSoAHost &&) = default;
+      DataSoAHost &operator=(DataSoAHost &&) = default;
+
+      explicit DataSoAHost(const SiStripQuality& quality, const SiStripGain* gains, const SiStripNoises& noises, DetToFeds& detToFeds, cudaStream_t stream)
+          : cms::cuda::PortableHostCollection<DataLayout>(sistrip::NUMBER_OF_FEDS * sistrip::FEDCH_PER_FED, stream) {
+        // connected: map<DetID, std::vector<int>>
+        // map of KEY=detid DATA=vector of apvs, maximum 6 APVs per detector module :
+        const auto& connected = quality.cabling()->connected();
+        // detCabling: map<DetID, std::vector<const FedChannelConnection *>
+        // map of KEY=detid DATA=vector<FedChannelConnection>
+        const auto& detCabling = quality.cabling()->getDetCabling();
+        
+        for (const auto& conn : connected) {
+          const auto det = conn.first;
+          if (!quality.IsModuleBad(det)) {
+            const auto detConn_it = detCabling.find(det);
+      
+            if (detCabling.end() != detConn_it) {
+              for (const auto& chan : (*detConn_it).second) {
+                if (chan && chan->fedId() && chan->isConnected()) {
+                  const auto detID = chan->detId();
+                  const auto fedID = chan->fedId();
+                  const auto fedCh = chan->fedCh();
+                  const auto iPair = chan->apvPairNumber();
+      
+                  detToFeds.emplace_back(detID, iPair, fedID, fedCh);
+      
+                  view().detID_()[channelIndex(fedID, fedCh)] = detID;
+                  view().iPair_()[channelIndex(fedID, fedCh)] = iPair;
+                  setInvThickness(fedID, fedCh, siStripClusterTools::sensorThicknessInverse(detID));
+      
+                  auto offset = 256 * iPair;
+                  
+                  for (auto strip = 0; strip < 256; ++strip) {
+                    const auto gainRange = gains->getRange(det);
+                    
+                    const auto detstrip = strip + offset;
+                    const std::uint16_t noise = SiStripNoises::getRawNoise(detstrip, noises.getRange(det));
+                    const auto gain = SiStripGain::getStripGain(detstrip, gainRange);
+                    const auto bad = quality.IsStripBad(quality.getRange(det), detstrip);
+                    
+                    // gain is actually stored per-APV, not per-strip
+                    setStrip(fedID, fedCh, detstrip, noise, gain, bad);
+                  }
+                }
+              }
+            }
+          }
+        }
+        
+        std::sort(detToFeds.begin(), detToFeds.end(), [](const DetToFed& a, const DetToFed& b) {
+          return a.detID() < b.detID() || (a.detID() == b.detID() && a.pair() < b.pair());
+        });
+      }
+      
+    private:
+      void setStrip(fedId_t fed, fedCh_t channel, stripId_t strip, std::uint16_t noise, float gain, bool bad) {
+        view().gain_()[apvIndex(fed, channel, strip)] = gain;
+        view().noise_()[stripIndex(fed, channel, strip)] = noise;
+        if (bad) {
+          view().noise_()[stripIndex(fed, channel, strip)] |= badBit;
+        }
+      }
+
+      void setInvThickness(fedId_t fed, fedCh_t channel, float invthick) {
+        view().invthick_()[channelIndex(fed, channel)] = invthick;
+      }
+    };
+
+  //  explicit SiStripClusterizerConditionsSoAHost(const SiStripQuality& quality,
+  //                                               const SiStripGain* gains,
+  //                                               const SiStripNoises& noises,
+  //                                               cudaStream_t stream) {};
+    //    : data_(quality, gains, noises, stream) {};
+
+  //  ~SiStripClusterizerConditionsSoAHost() = default;
+
+    //DataSoAHost data() const { return data_; }
+
+  //private:
+  //  DetToFeds detToFeds_;
+  //  //DataSoAHost data_;
+
+  //};
 
 }  // namespace stripgpu
 
--- CalibFormats/SiStripObjects/interface/SiStripClusterizerConditionsSoAUtilities.h
+++ CalibFormats/SiStripObjects/interface/SiStripClusterizerConditionsSoAUtilities.h
@@ -2,44 +2,37 @@
 #define CalibFormats_SiStripObjects_SiStripClusterizerConditionsSoAUtilities_h
 
 #include "DataFormats/SiStripCluster/interface/SiStripTypes.h"
+#include "DataFormats/SiStripCommon/interface/ConstantsForHardwareSystems.h"
 #include "DataFormats/SoATemplate/interface/SoALayout.h"
 
-#include "DataFormats/SiStripCommon/interface/ConstantsForHardwareSystems.h"
-
-#include "HeterogeneousCore/CUDACore/interface/ESProduct.h"
-//#include "HeterogeneousCore/CUDAUtilities/interface/cudaCompat.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/device_unique_ptr.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/host_unique_ptr.h"
 #include "HeterogeneousCore/CUDAUtilities/interface/HostAllocator.h"
 
-struct DetToFedSoA {
-  GENERATE_SOA_LAYOUT(DetToFedSoALayout,
-		      SOA_COLUMN(stripgpu::detId_t, detid_),
-                      SOA_COLUMN(stripgpu::avpPair_t, ipair_),
-                      SOA_COLUMN(stripgpu::fedId_t, fedid_),
-                      SOA_COLUMN(stripgpu::fedCh_t, fedch_));
-  
-};
+//struct DetToFedSoA {
+//  GENERATE_SOA_LAYOUT(DetToFedSoALayout,
+//		                  SOA_COLUMN(stripgpu::detId_t, detID_),
+//                      SOA_COLUMN(stripgpu::apvPair_t, pair_),
+//                      SOA_COLUMN(stripgpu::fedId_t, fedID_),
+//                      SOA_COLUMN(stripgpu::fedCh_t, fedCh_));
+//};
+//
+//using DetToFedLayout = typename DetToFedSoA::DetToFedSoALayout<>;
+//using DetToFedView = typename DetToFedSoA::DetToFedSoALayout<>::View;
+//using DetToFedConstView = typename DetToFedSoA::DetToFedSoALayout<>::ConstView;
 
-using DetToFedLayout = typename DetToFedSoA::DetToFedSoALayout<>;
-using DetToFedView = typename DetToFedSoA::DetToFedSoALayout<>::View;
-using DetToFedConstView = typename DetToFedSoA::DetToFedSoALayout<>::ConstView;
-
-struct Data {
+struct DataSoA {
+  using arraySTRIPS_PER_FEDCH = std::array<std::uint16_t,sistrip::STRIPS_PER_FEDCH>;
+  using arrayAPVS_PER_FEDCH = std::array<float,sistrip::APVS_PER_FEDCH>;
   GENERATE_SOA_LAYOUT(DataSoALayout,
-		      SOA_COLUMN(std::uint16_t, noise_),
+		                  //SOA_COLUMN(arraySTRIPS_PER_FEDCH, noise_),
+		                  SOA_COLUMN(std::uint16_t, noise_),
                       SOA_COLUMN(float, invthick_),
+                      SOA_COLUMN(stripgpu::detId_t, detID_),
                       SOA_COLUMN(stripgpu::apvPair_t, iPair_),
+                      //SOA_COLUMN(arrayAPVS_PER_FEDCH, gain_));
                       SOA_COLUMN(float, gain_));
-
 };
-/*
-const std::uint16_t* noise_;  //[sistrip::NUMBER_OF_FEDS*sistrip::FEDCH_PER_FED * sistrip::STRIPS_PER_FEDCH];
-const float* invthick_;       //[sistrip::NUMBER_OF_FEDS*sistrip::FEDCH_PER_FED];
-const detId_t* detID_;        //[sistrip::NUMBER_OF_FEDS*sistrip::FEDCH_PER_FED];
-const apvPair_t* iPair_;      //[sistrip::NUMBER_OF_FEDS*sistrip::FEDCH_PER_FED];
-const float* gain_;           //[sistrip::NUMBER_OF_FEDS*sistrip::APVS_PER_FEDCH * sistrip::FEDCH_PER_FED];
-*/
 
 using DataLayout = typename DataSoA::DataSoALayout<>;
 using DataView = typename DataSoA::DataSoALayout<>::View;
@@ -50,45 +43,21 @@ class SiStripGain;
 class SiStripNoises;
 
 namespace stripgpu {
-  __host__ __device__ inline fedId_t fedIndex(fedId_t fed) { return fed - sistrip::FED_ID_MIN; }
-  __host__ __device__ inline std::uint32_t stripIndex(fedId_t fed, fedCh_t channel, stripId_t strip) {
-    return fedIndex(fed) * sistrip::FEDCH_PER_FED * sistrip::STRIPS_PER_FEDCH + channel * sistrip::STRIPS_PER_FEDCH +
-           (strip % sistrip::STRIPS_PER_FEDCH);
-  }
-  __host__ __device__ inline std::uint32_t apvIndex(fedId_t fed, fedCh_t channel, stripId_t strip) {
-    return fedIndex(fed) * sistrip::APVS_PER_FEDCH * sistrip::FEDCH_PER_FED + sistrip::APVS_PER_CHAN * channel +
-           (strip % sistrip::STRIPS_PER_FEDCH) / sistrip::STRIPS_PER_APV;
-  }
-  __host__ __device__ inline std::uint32_t channelIndex(fedId_t fed, fedCh_t channel) {
-    return fedIndex(fed) * sistrip::FEDCH_PER_FED + channel;
-  }
-
   static constexpr std::uint16_t badBit = 1 << 15;
-  __device__ inline detId_t detID(fedId_t fed, fedCh_t channel) const {
-    return detID_[channelIndex(fed, channel)];
-  }
-  
-  __device__ inline apvPair_t iPair(fedId_t fed, fedCh_t channel) const {
-    return iPair_[channelIndex(fed, channel)];
-  }
-  
-  __device__ inline float invthick(fedId_t fed, fedCh_t channel) const {
-    return invthick_[channelIndex(fed, channel)];
-  }
-  
-  __device__ inline float noise(fedId_t fed, fedCh_t channel, stripId_t strip) const {
-    // noise is stored as 9 bits with a fixed point scale factor of 0.1
-    return 0.1f * (noise_[stripIndex(fed, channel, strip)] & ~badBit);
-  }
-  
-  __device__ inline float gain(fedId_t fed, fedCh_t channel, stripId_t strip) const {
-    return gain_[apvIndex(fed, channel, strip)];
-  }
-  
-  __device__ inline bool bad(fedId_t fed, fedCh_t channel, stripId_t strip) const {
-    return badBit == (noise_[stripIndex(fed, channel, strip)] & badBit);
-  }
-  
+//
+//  __host__ __device__ inline fedId_t fedIndex(fedId_t fed) { return fed - sistrip::FED_ID_MIN; }
+//  __host__ __device__ inline std::uint32_t stripIndex(fedId_t fed, fedCh_t channel, stripId_t strip) {
+//    return fedIndex(fed) * sistrip::FEDCH_PER_FED * sistrip::STRIPS_PER_FEDCH + channel * sistrip::STRIPS_PER_FEDCH +
+//           (strip % sistrip::STRIPS_PER_FEDCH);
+//  }
+//  __host__ __device__ inline std::uint32_t apvIndex(fedId_t fed, fedCh_t channel, stripId_t strip) {
+//    return fedIndex(fed) * sistrip::APVS_PER_FEDCH * sistrip::FEDCH_PER_FED + sistrip::APVS_PER_CHAN * channel +
+//           (strip % sistrip::STRIPS_PER_FEDCH) / sistrip::STRIPS_PER_APV;
+//  }
+//  __host__ __device__ inline std::uint32_t channelIndex(fedId_t fed, fedCh_t channel) {
+//    return fedIndex(fed) * sistrip::FEDCH_PER_FED + channel;
+//  }
+//
 }  // namespace stripgpu
 
 #endif
