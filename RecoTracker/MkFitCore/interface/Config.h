be2ec88e4fe0a2ba231d3bce9c0e5b44de1b0527
Matevz Tadel <mtadel@ucsd.edu>
2024-08-23 14:30:34 -0700

    Make Config::usePropToPlane a commandline-option for mkFit: --use-p2p 0 or 1. No longer constexpr so we can compare profiling runs on the same build.

--- RecoTracker/MkFitCore/interface/Config.h
+++ RecoTracker/MkFitCore/interface/Config.h
@@ -49,7 +49,10 @@ namespace mkfit {
     // Config for propagation - could/should enter into PropagationFlags?!
     constexpr int Niter = 5;
     constexpr bool useTrigApprox = true;
-    constexpr bool usePropToPlane = true;
+    // Move to Config.cc, make a command-line option in mkFit.cc to ease profiling comparisons.
+    // If making this constexpr agin, also fix ifs using it in MkBuilder.cc and MkFinder.cc.
+    // constexpr bool usePropToPlane = true;
+    extern bool usePropToPlane;
     constexpr bool usePtMultScat = false;
 
     // Config for Bfield. Note: for now the same for CMS-phase1 and CylCowWLids.
