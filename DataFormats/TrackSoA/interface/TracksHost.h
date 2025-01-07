f0f975e9999848b0513b273bc49e270a2017d2d1
Brunella DAnzi <brunella.danzi@cern.ch>
2025-01-07 06:48:26 -0800

    Rebase to 15_0_0_pre1 almost complete

--- DataFormats/TrackSoA/interface/TracksHost.h
+++ DataFormats/TrackSoA/interface/TracksHost.h
@@ -7,6 +7,7 @@
 #include "DataFormats/TrackSoA/interface/TracksSoA.h"
 #include "DataFormats/TrackSoA/interface/TrackDefinitions.h"
 #include "DataFormats/Portable/interface/PortableHostCollection.h"
+#include "Geometry/CommonTopologies/interface/SimplePixelStripTopology.h"
 
 // TODO: The class is created via inheritance of the PortableHostCollection.
 // This is generally discouraged, and should be done via composition.
@@ -36,6 +37,7 @@ namespace pixelTrack {
   using TracksHostPhase1 = TracksHost<pixelTopology::Phase1>;
   using TracksHostPhase2 = TracksHost<pixelTopology::Phase2>;
   using TracksHostHIonPhase1 = TracksHost<pixelTopology::HIonPhase1>;
+  using TracksHostPhase1Strip = TracksHost<pixelTopology::Phase1Strip>;
 
 }  // namespace pixelTrack
 
