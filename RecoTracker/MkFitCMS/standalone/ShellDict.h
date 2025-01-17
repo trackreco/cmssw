a5e96e2d4807dbfbbd1528f19e4e986342b6ffc7
Matevz Tadel <mtadel@ucsd.edu>
2025-01-16 23:26:30 -0800

    Exploration of phase2 seed-quality, selectHitIndicesV2 improvements and explicit PS double layer handling.
    * Shell:
      - Seed preselection
      - Honor --num-iters
      - In ProcessEvent() temporarily hardcode call to findTracksStandardv2p2()
        instead of findTracksCloneEngine(). To be made configurable, somehow.
    
    * MkBuilder
      - Sketch-up findTracksStandardv2p2() to fiddle with the PS layer processing
        and selectHitIndicesV2().
    
    * MkFinder
      - Extract struct Bins out of selectHitIndicesV2() so it can be used
        in different contexts.
      - Implement  selectHitIndicesV2p2() - a variant of selectHitIndicesV2().
    
    * SteeringParams
      - Implement fill_plan_swap_pairs() to simplify PS layer swapping.
    
    * Makefile.config
      - Separate out CXXFLAGS Werror=array-bounds - it needs to be disabled
        for MPT_SIZE=1 for some reason.
      - Add separatevariable CXX_STD := 20 to make it easier to re-set it
        to the same version ROOT was built with when building WITH_ROOT.

--- RecoTracker/MkFitCMS/standalone/ShellDict.h
+++ RecoTracker/MkFitCMS/standalone/ShellDict.h
@@ -2,6 +2,7 @@
 #include "RecoTracker/MkFitCore/standalone/Event.h"
 #include "RecoTracker/MkFitCore/standalone/ConfigStandalone.h"
 #include "RecoTracker/MkFitCore/interface/TrackerInfo.h"
+#include "RecoTracker/MkFitCore/interface/MkBuilder.h"
 
 #include "RecoTracker/MkFitCore/interface/MatrixSTypes.h"
 
